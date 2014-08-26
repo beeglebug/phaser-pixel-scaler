82048c3b78cc934abbd4d0568f7ac1014e4dd23f
Stuart Lee <stuart.lee@drpgroup.com>
2014-08-26 09:33:49 +0100

    initial commit

---
+++ PixelScaler.js
@@ -0,0 +1,49 @@
+Phaser.Plugin.PixelScaler = function(game, parent) {
+
+    Phaser.Plugin.call(this, game, parent);
+
+};
+
+Phaser.Plugin.PixelScaler.prototype = Object.create(Phaser.Plugin.prototype);
+
+
+Phaser.Plugin.PixelScaler.prototype.init = function(scale) {
+
+    this.scale = scale;
+
+    this.originalWidth = this.game.width;
+    this.originalHeight = this.game.height;
+
+    this.scaledWidth = this.originalWidth * this.scale;
+    this.scaledHeight = this.originalHeight * this.scale;
+
+    // create a new scaled canvas to draw to
+    this.scaledCanvas = Phaser.Canvas.create(this.scaledWidth, this.scaledHeight);
+    this.context = this.scaledCanvas.getContext('2d');
+
+    Phaser.Canvas.addToDOM(this.scaledCanvas);
+    Phaser.Canvas.setSmoothingEnabled(this.context, false);
+
+    this.originalCanvas = this.game.canvas;
+
+    this.originalCanvas.style['display'] = 'none';
+
+};
+
+
+/**
+ * cop the original canvas to the new canvas
+ */
+Phaser.Plugin.PixelScaler.prototype.postRender = function() {
+
+    this.context.drawImage(
+        this.originalCanvas,
+        0, 0,
+        this.originalWidth,
+        this.originalHeight,
+        0, 0,
+        this.scaledWidth,
+        this.scaledHeight
+    );
+
+};
